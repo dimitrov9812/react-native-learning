67e89e6822456ed681f3aaecd1eb3acbaca6a7de
dimitrov9812 <alexanderdimitrov333@gmail.com>
2020-06-05 16:10:58 +0300

    Explored the file structure and created new component header to render. Created it exported it and then after the import in the root of the app we rendered it successfully.

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-/**
- * @format
- */
-
-import {AppRegistry} from 'react-native';
-import App from './App';
+// Import a library to help create a component.
+import React from 'react';
+import {Text, AppRegistry} from 'react-native';
 import {name as appName} from './app.json';
+import Header from '../firstProject/src/components/Header';
 
+// Create a component.
+const App = () => {
+    return(
+        <>
+            <Header/>
+            <Text>Outside Header</Text>
+        </>
+    )
+}
+
+// Render it to the device.
 AppRegistry.registerComponent(appName, () => App);
---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+const Header = () => {
+    return <Text>Yu-Gi-Oh - Deck Builder</Text>
+};
+
+export default Header;
